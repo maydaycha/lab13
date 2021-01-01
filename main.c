47dc533b5a1bc91db24597ba770e1052cbb4fb7f
jason.chiu <jason.chiu@realtek.com>
2021-01-02 01:11:10 +0800

    fix crash when free()

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #define MAX_LEN_EACH_DECODE   3
 
 
-char *__decode_4char(char *e, char *d);
+void __decode_4char(char *e, char *d);
 
 // num_of_char : number of original characters
 // e_string : encoded string
@@ -23,29 +23,29 @@ int decode(char *e_string, int e_len)
 {
     int n_ori_char = (int)e_string[0] - ASCII_LEN_BASE;
     char *s_decoded;
-    int i = 0;
+    int i = 0, j = 0;
 
 #if 0
     printf("n_ori_char:%d\n", n_ori_char);
 #endif
 
-    s_decoded = (char *)malloc(n_ori_char + 1); // need add '\0' in the tail
+    s_decoded = (char *)malloc(n_ori_char + 1); // need add '\0' in the tail ?
     if (!s_decoded)
         return -1;
 
-    for (i = 0; i < e_len; i+= MAX_LEN_EACH_ENCODE) {
-        __decode_4char(&e_string[1 + i], &s_decoded[i]);
+    for (i = 0, j = 0; i < e_len; i+=MAX_LEN_EACH_ENCODE, j+=MAX_LEN_EACH_DECODE) {
+        __decode_4char(&e_string[1 + i], &s_decoded[j]);
     }
 
 #if 0
     printf("\n\n\n");
 #endif
-    /*free(s_decoded);*/
+    free(s_decoded);
 
     return 0;
 }
 
-char *__decode_4char(char *e, char *d)
+void __decode_4char(char *e, char *d)
 {
     char c1 = e[0], c2 = e[1], c3 = e[2], c4 = e[3];
 
@@ -58,7 +58,6 @@ char *__decode_4char(char *e, char *d)
     d[1] = ((c3 >> 2) & 0xF) | ((c2 & 0xF) << 4);
     d[0] = ((c2 >> 4) & 0x3) | ((c1 & 0x3F) << 2);
     printf("%c%c%c", d[0], d[1], d[2]);
-    return d;
 }
 
 
