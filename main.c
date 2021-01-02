6f559901a972f22d802a95407d4db220a1f8e712
maydaycha <maydaychaaaa@gmail.com>
2021-01-02 16:04:19 +0800

    add comment

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
-// FIXME: need to change to _THIS_YEAR with test case of this year.
 #define EXTRA_CHAR ' '
 #define ENCODE_CHAR ' '
 #define ASCII_LEN_BASE  '0'
@@ -10,8 +10,14 @@
 #define MAX_LEN_EACH_DECODE   3
 
 
-void __decode_4char(char *e, char *d);
+void __decode_4char(char *e, char *d);  // function declartion.
 
+/*
+ * to get the nearest "multiple" value of from "numToRound.
+ * @numtoRound : value to round up
+ * @multiple : base value to multiple
+ *
+ */
 static int roundUp(int numToRound, int multiple)
 {
     if (multiple == 0)
@@ -25,9 +31,14 @@ static int roundUp(int numToRound, int multiple)
 }
 
 
+/*
+ * decode the string based on description
+ * @e_string : encoded string.
+ * @e_len : length of encoded string
+ * */
 int decode(char *e_string, int e_len)
 {
-    int n_ori_char = (int)e_string[0] - ASCII_LEN_BASE;
+    int n_ori_char = (int)e_string[0] - ASCII_LEN_BASE; // decode the value of length of chars.
     char *s_decoded;
     int i = 0, j = 0;
     size_t size_alloc = roundUp(n_ori_char, MAX_LEN_EACH_DECODE);  // we allocat multuple of "MAX_LEN_EACH_DECODE" to store decoded chars including "extra char for encoding"
@@ -39,7 +50,7 @@ int decode(char *e_string, int e_len)
     memset(s_decoded, '\0', size_alloc);
 
     for (i = 0, j = 0; i < e_len; i += MAX_LEN_EACH_ENCODE, j += MAX_LEN_EACH_DECODE) {
-        __decode_4char(&e_string[1 + i], &s_decoded[j]);
+        __decode_4char(&e_string[1 + i], &s_decoded[j]);  // dececode 4 encoded chars, follow the description in pdf.
     }
     s_decoded[n_ori_char] = '\0';  // to prevent printf %s to print "extra char" added when encode.
 
@@ -61,6 +72,9 @@ void __decode_4char(char *e, char *d)
     c3 -= ENCODE_CHAR;
     c4 -= ENCODE_CHAR;
 
+    /* decode each encoded char base on description in pdf.
+     * bitwise operation for four encoded chars to three decoded chars
+     * */
     d[2] = (c4 & 0x3F) | ((c3 & 0x3) << 6);
     d[1] = ((c3 >> 2) & 0xF) | ((c2 & 0xF) << 4);
     d[0] = ((c2 >> 4) & 0x3) | ((c1 & 0x3F) << 2);
@@ -75,14 +89,7 @@ int main(int argc, const char *argv[])
     size_t len = 0;
     ssize_t read;
 
-    while ((read = getline(&line, &len, stdin)) != -1) {
-#if 0
-        printf("Retrieved line of length %zu (oc len=%d, encode_len=%d) :\n",
-                read,
-                (line[0] - 48),
-                read - 1 - 1);
-        printf("%s", line);
-#endif
+    while ((read = getline(&line, &len, stdin)) != -1) {  // read each line of string from standard input
         decode(line, read);
     }
     free(line);
